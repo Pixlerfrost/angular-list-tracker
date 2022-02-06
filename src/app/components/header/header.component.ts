d52c8ae49ff4af39e6086caa7086fa8593d787fb
Mohamad Youness <pixlerfrost@gmail.com>
2022-02-05 20:36:42 -0500

    add task toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from '../../services/ui.service';
+
 
 @Component({
   selector: 'app-header',
@@ -6,14 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  title: string = 'Task Tracker'
+  showAddTask: boolean = false;
+  subscription: any; 
 
-  constructor() { }
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle')
+    this.uiService.toggleAddTask();
   }
 
 }
